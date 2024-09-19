387a8cafda6bf9648ef40d57b7917b270ed0aa6b
L14MtheC0der <110752143+L14MtheC0der@users.noreply.github.com>
2024-09-18 20:31:02 -0500

    Again

---
+++ src/components/BabylonExamples.vue
@@ -0,0 +1,42 @@
+<template>
+  
+    <h3>Babylon Examples</h3>
+    <canvas></canvas>
+    
+ 
+  
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { FpController } from '@/BabylonExamples/Controller';
+export default defineComponent({
+  name: 'BabylonExamples',
+  mounted(){
+    const canvas = document.querySelector('canvas')!
+    
+    
+    
+
+
+    new FpController(canvas)
+  }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Roboto:wght@100;700&display=swap");
+
+
+canvas {
+  width:100% ;
+  height: 100%;
+
+  color: blue;
+}
+
+
+
+</style>
